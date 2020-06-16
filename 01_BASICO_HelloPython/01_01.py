f2afe5eaef23c060e65bb3d4f182ee7e992e06f1
Sérgio Pires <slpires@slpires.com>
2020-06-15 23:14:12 -0300

    Aula 02 (BÁSICO) - Números (e variáveis).

--- 01_BASICO_HelloPython/01_01.py
+++ 01_BASICO_HelloPython/01_01.py
@@ -1,7 +1,11 @@
 # Comentários em um linha.
 
-'''
+"""
 Bloco de comentários.
+"""
+
+'''
+Bloco de comentário
 '''
 
 print("Hello Python!")
---
+++ 02_BASICO_Numeros/02_01.py
@@ -0,0 +1,52 @@
+"""
+Sobre variáveis:
+1 - sempre começam com letra minúscula (PEP8);
+2 - não podem começar com números;
+3 - não pode haver espaços no nome (use "_");
+4 - caracteres especiais não são permitidos.
+"""
+
+
+# Números em Python
+
+"""
+Tipos principais de números:
+- int (Ex: 1, 10, -5, ...)
+- float (Ex: 1.2, -0.5, 2e2, 3E2, ...)
+"""
+
+# Aritmética básica
+
+soma = 2 + 1
+print("soma = ", soma)
+
+subtracao = 2 - 1
+print("subtracao = ", subtracao)
+
+multiplicacao = 2 * 2
+print("multiplicacao = ", multiplicacao)
+
+# Divisão sempre retorna um float (divisão de ponto flutuante).
+divisao1 = 2 / 1
+print("divisao1 = ", divisao1)
+
+divisao2 = 3 / 2
+print("divisao2 = ", divisao2)
+
+divisao3 = 6 / 2.0
+print("divisao3 = ", divisao3)
+
+# Divisão inteira.
+divisao4 = 3 // 2
+print("divisao4 = ", divisao4)
+
+restoDivisao = 10 % 3
+print("restoDivisao = ", restoDivisao)
+
+exponenciacao = 3 ** 2
+print("exponenciacao = ", exponenciacao)
+
+radiciacao = 4 ** 0.5
+print("radiciacao = ", radiciacao)
+
+
