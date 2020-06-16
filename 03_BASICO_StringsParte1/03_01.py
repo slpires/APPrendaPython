499478633ee5fdd1d12683cd4afae00160b64fdc
Sérgio Pires <slpires@slpires.com>
2020-06-16 00:16:39 -0300

    Aula 03 (BÁSICO) - Strings - Parte 1.

---
+++ 03_BASICO_StringsParte1/03_01.py
@@ -0,0 +1,20 @@
+# Strings sempre serão vistas como uma seguência de caracteres.
+# Isso permite utilizarmos índices para capturar partes de uma string.
+
+string1 = 'Teste 1'
+print(string1)
+
+string2 = "Teste 2"
+print(string2)
+
+# Testes de impressão.
+
+print("Primeira linha.")
+print("Segunda linha.")
+print("Terceira linha \n Quarta linha \n  Quinta linha.")
+
+print("Sexta linha", end=" (não pula linha)")
+print("... continuação da sexta linha.")
+print("Sétima linha.")
+
+print("inteiro = %d, float = %f, string = %s" %(1.00, 2, 3))
---
+++ 03_BASICO_StringsParte1/03_02.py
@@ -0,0 +1,61 @@
+# Strings sempre serão vistas como uma seguência de caracteres.
+# Isso permite utilizarmos índices para capturar partes de uma string.
+
+# Comprimento de uma string
+s = "Hello World"
+print(s)
+
+comprimento = len(s)
+print(comprimento)
+
+# Indexação
+print(s[0], s[1],  s[2],   s[3],    s[4],     s[5],      s[6], s[7], s[8], s[9], s[10])
+
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[5])
+print(s[6])
+print(s[7])
+print(s[8])
+print(s[9])
+print(s[10])
+
+# Intervalos
+
+print(s)
+print(s[1:])
+print(s[:3])
+print(s[1:4])
+
+# OBS: O limite final nunca é incluído.
+
+print(s)
+print(s[-1])
+print(s[-2])
+print(s[-3])
+print(s[-4])
+print(s[-5])
+print(s[-6])
+print(s[-7])
+print(s[-8])
+print(s[-9])
+print(s[-10])
+print(s[-11])
+
+# Imprimindo tudo utilizando espaçamento
+print(s)
+print(s[::1])
+print(s[::2])
+print(s[::3])
+print(s[::4])
+print(s[::5])
+print(s[::6])
+
+print(s)
+print(s[::-1])
+print(s[::-2])
+print(s[::-3])
+
